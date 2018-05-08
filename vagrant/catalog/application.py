bd21d3b34b768415109ae08444157a9e937a6254
Yusuke Asai <yu.yu.jp0@gmail.com>
2018-05-08 23:05:54 +0900

    Add imports

---
+++ vagrant/catalog/application.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Company, Smartphone, User
+from flask import session as login_session
+import random
+import string
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+
+app = Flask(__name__)
