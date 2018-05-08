30653f1510182c71fd19168bebd0613fde46b755
Yusuke Asai <yu.yu.jp0@gmail.com>
2018-05-08 22:59:03 +0900

    Code to add a user and a company data to DB

---
+++ vagrant/catalog/addmanyitems.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Company, Base, Smartphone, User
+
+engine = create_engine('sqlite:///companysmartphone.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+# Create dummy user
+User1 = User(name="Yusuke Asai", email="yuyu@udacity.com",
+             picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png')
+session.add(User1)
+session.commit()
+
+company1 = Company(user_id=1, name="Apple")
+
+session.add(company1)
+session.commit()
