b7da220205ef7acfbfcb3167e0c45fc92a5aacb9
Yusuke Asai <yu.yu.jp0@gmail.com>
2018-05-08 22:46:43 +0900

    Add User class

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -5,6 +5,13 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
 
 engine = create_engine('sqlite:///companysmartphone.db')
 
