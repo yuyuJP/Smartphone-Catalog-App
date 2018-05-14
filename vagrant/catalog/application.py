c09683d75faef8956a029b50986919a9b43139c2
Yusuke Asai <yu.yu.jp0@gmail.com>
2018-05-15 02:54:38 +0900

    Delete unnecessary ignore mark

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -61,7 +61,7 @@ def gconnect():
 
     # Check that the access token is valid.
     access_token = credentials.access_token
-    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'  # NOQA
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
            % access_token)
     h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
