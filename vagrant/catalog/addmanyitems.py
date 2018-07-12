8ba930a524e8ba3131ac52d43be4eefeb920def1
Yusuke Asai <yu.yu.jp0@gmail.com>
2018-07-12 14:51:45 +0900

    Change database path for postgresql

--- vagrant/catalog/addmanyitems.py
+++ vagrant/catalog/addmanyitems.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from database_setup import Company, Base, Smartphone, User
 
-engine = create_engine('sqlite:///companysmartphone.db')
+engine = create_engine('postgresql://catalog:catalogpassword@localhost/catalog')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
