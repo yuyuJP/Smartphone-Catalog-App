3b9fe3799db969f8974da5e865c5ff6c1f906f28
Yusuke Asai <yu.yu.jp0@gmail.com>
2018-07-12 16:30:36 +0900

    Change file path

--- vagrant/catalog/__init__.py
+++ vagrant/catalog/__init__.py
@@ -15,7 +15,7 @@ import requests
 app = Flask(__name__)
 
 CLIENT_ID = json.loads(
-    open('client_secrets.json', 'r').read())['web']['client_id']
+    open('/var/www/catalog/catalog/client_secrets.json', 'r').read())['web']['client_id']
 APPLICATION_NAME = "Smartphone Catalog App"
 
 # Connect to Database and create database session
@@ -50,7 +50,7 @@ def gconnect():
 
     try:
         # Upgrade the authorization code into a credentials object
-        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow = flow_from_clientsecrets('/var/www/catalog/catalog/client_secrets.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
